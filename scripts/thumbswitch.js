6699035be0a48129c57e15eefdd8a24bc9a94453
Electro-Corp <chinmaytanmay@outlook.com>
2024-07-03 17:08:09 -0600

    init

---
+++ scripts/thumbswitch.js
@@ -0,0 +1,28 @@
+/*
+    waitForEl(el) is a modified version of: https://stackoverflow.com/a/60062546
+*/
+function waitForEl(el) {
+    return new Promise((resolve, reject) => {
+        const intervalId = setInterval(() => {
+        console.log("polling!!")
+        if (document.querySelectorAll(el).length > 0) {
+            clearInterval(intervalId);
+            resolve();
+        }
+        }, 500);
+    });
+}
+
+function randomize(){
+    waitForEl(".yt-core-image").then(() => {
+        const images = document.querySelectorAll(".yt-core-image")
+
+        if(images){
+            console.log(images.length)
+        }
+        console.log("done!!")
+        randomize();
+    });
+}
+
+
