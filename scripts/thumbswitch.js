edb38531a4c192a67a81b0b5114beb87a6ed06ac
Electro-Corp <chinmaytanmay@outlook.com>
2024-07-03 17:20:24 -0600

    kind of works, lots of issues

--- scripts/thumbswitch.js
+++ scripts/thumbswitch.js
@@ -5,7 +5,7 @@ function waitForEl(el) {
     return new Promise((resolve, reject) => {
         const intervalId = setInterval(() => {
         console.log("polling!!")
-        if (document.querySelectorAll(el).length > 0) {
+        if (document.querySelectorAll(el).length > 10) {
             clearInterval(intervalId);
             resolve();
         }
@@ -13,21 +13,47 @@ function waitForEl(el) {
     });
 }
 
+
 function randomize(){
     waitForEl(".yt-core-image").then(() => {
-        const images = document.querySelectorAll(".yt-core-image")
+        const images = document.querySelectorAll(".yt-core-image");
 
         if(images){
-            console.log(images.length)
+            console.log(images.length);
         }
 
+        imagesources = [];
+
         images.forEach(img => {
-            console.log(img.src)
+            imagesources.push(img.src);
         });
 
-        console.log("done!!")
+        images.forEach(img => {
+            src = null
+            while(!src){
+                num = randInRange(0, imagesources.length)
+                src = imagesources[num];
+                console.log(num)
+                console.log(src);   
+            }
+            img.src = src;
+        });
+
+        
+
+
+        console.log("done!!");
         //randomize();
     });
+}
+
+/*
+    randInRange from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+*/
+function randInRange(min, max){
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
 
